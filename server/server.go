7ae1df927fb9758cc52f005140f4d01ec04ba146
Goberman15 <akbarrmdhn94@gmail.com>
2024-02-17 07:15:25 +0700

    Implement Logger, Auth, and Error Handler

--- server/server.go
+++ server/server.go
@@ -1,7 +1,12 @@
 package server
 
 import (
+	"errors"
+
+	"github.com/goberman15/sandbox-l4/customErr"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/basicauth"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/jmoiron/sqlx"
 )
 
@@ -11,7 +16,18 @@ type Server struct {
 }
 
 func NewServer(db *sqlx.DB) *Server {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		ErrorHandler: errorHandler,
+	})
+	app.Use(logger.New())
+	app.Use(basicauth.New(basicauth.Config{
+		Users: map[string]string{
+			"sandbox": "unbroken",
+		},
+		Unauthorized: func(c *fiber.Ctx) error {
+			return customErr.NewUnauthorizedError("you are not allowed to perform this action")
+		},
+	}))
 	return &Server{
 		app: app,
 		db:  db,
@@ -19,5 +35,16 @@ func NewServer(db *sqlx.DB) *Server {
 }
 
 func (s *Server) Start() error {
-	return s.app.Listen(":3000")
+	return s.app.Listen(":8080")
+}
+
+func errorHandler(ctx *fiber.Ctx, err error) error {
+	var e customErr.CustomError
+	if errors.As(err, &e) {
+		return ctx.Status(e.StatusCode()).JSON(fiber.Map{
+			"message": e.Error(),
+		})
+	}
+
+	return fiber.NewError(fiber.StatusInternalServerError, err.Error())
 }
